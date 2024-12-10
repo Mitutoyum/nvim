669360e74a48994f528d523bb796da9a876b8ab3
Mitutoyo <mitutoyum@proton.me>
2024-12-10 12:50:16 +0700

    New keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,24 @@
 local map = vim.keymap.set
 
+-- Navigate windows
 map("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Resize windows
+map("n", "<C-Up>", ":resize -2<CR>")
+map("n", "<C-Down>", ":resize +2<CR>")
+map("n", "<C-Left>", ":vertical resize -2<CR>")
+map("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Navigate block
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected block down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected block up" })
+
+-- Indent
+map("n", ">", ">>")
+map("n", "<", "<<")
+
+map("v", ">", ">gv")
+map("v", "<", "<gv")
