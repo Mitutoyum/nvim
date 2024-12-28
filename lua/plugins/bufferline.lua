a85f78980284c1ec77c4280929523e4fb865a956
Mitutoyo <mitutoyum@proton.me>
2024-12-28 23:07:40 +0700

    Added keymaps

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,19 +2,24 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function()
-		require("bufferline").setup({
-			options = {
-				offsets = {
-					{
-						filetype = "neo-tree",
-						text = "Neo-tree",
-						text_align = "left",
-						highlight = "Directory",
-					},
+	opts = {
+		options = {
+			offsets = {
+				{
+					filetype = "neo-tree",
+					text = "Neo-tree",
+					text_align = "left",
+					highlight = "Directory",
 				},
-				diagnostics = "nvim_lsp",
 			},
-		})
+			diagnostics = "nvim_lsp",
+		},
+	},
+	config = function(_, opts)
+		require("bufferline").setup(opts)
+		local map = vim.keymap.set
+
+		map("n", "tn", "<cmd>BufferLineCycleNext<CR>", { desc = "Next tab" })
+		map("n", "tp", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous tab" })
 	end,
 }
