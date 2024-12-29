052f964b7b0b112abb9860bc1fe690d3b5b13ddb
Mitutoyo <mitutoyum@proton.me>
2024-12-29 19:39:47 +0700

    added telescope to dependencies

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,8 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
+
+		"nvim-telescope/telescope.nvim",
 	},
 	config = function()
 		local default_capabilities = require("cmp_nvim_lsp").default_capabilities()
